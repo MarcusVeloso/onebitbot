f3dcace00e8a519f88feeb3bcfc787d2852a2761
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-04-07 18:00:25 -0300

    add migrations

--- app.rb
+++ app.rb
@@ -4,8 +4,11 @@ require 'sinatra/activerecord'
   
 require './config/database'
   
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
+
 class App < Sinatra::Base
   get '/' do
-    'Hello world!'
+    'Hello world SUUUPER!'
   end
 end
