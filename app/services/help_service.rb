ec789fa1f3609b3c64be71c6a7c8b84f31e8757c
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-04-08 18:02:46 -0300

    update text format 4

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -1,18 +1,4 @@
 class HelpService
   def self.call
-    response  = ' **Meus comandos** \n\n'
-    response += 'help\n'
-    response += ' `Lista de comandos que eu conheço` \n\n'
-    response += 'Adicione ao Faq\n'
-    response += ' `Adiciona uma nova questão ao Faq` \n\n'
-    response += 'Remova ID\n'
-    response += ' `Remove uma questão baseada no ID dela` \n\n'
-    response += 'O que você sabe sobre X\n'
-    response += ' `Pesquisa por palavra na lista de perguntas e respostas` \n\n'
-    response += 'Pesquise a hashtag X\n'
-    response += ' `Lista as perguntas e respostas com aquela hashtag` \n\n'
-    response += 'Perguntas e Respostas\n'
-    response += ' `Mostra a lista de perguntas e respostas` \n\n'
-    response
-  end
-end
+    response  = " **Meus comandos** \n\n help\n `Lista de comandos que eu conheço` \n\n Adicione ao Faq\n `Adiciona uma nova questão ao Faq` \n\n Remova ID\n `Remove uma questão baseada no ID dela` \n\n O que você sabe sobre X\n `Pesquisa por palavra na lista de perguntas e respostas` \n\n Pesquise a hashtag X\n `Lista as perguntas e respostas com aquela hashtag` \n\n Perguntas e Respostas\n `Mostra a lista de perguntas e respostas` \n\n"
+    end
